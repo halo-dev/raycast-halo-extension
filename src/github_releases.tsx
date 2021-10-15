a929da7b2b9bd00d78989a56365425ac01027714
Ryan Wang <i@ryanc.cc>
2021-10-15 23:05:18 +0800

    feat: support search journals and create journal.

---
+++ src/github_releases.tsx
@@ -0,0 +1,180 @@
+import { useEffect, useState } from "react";
+import { HaloRestAPIClient } from "@halo-dev/admin-api";
+import { ActionPanel, Detail, Icon, ImageMask, List, OpenInBrowserAction, PushAction, showToast, ToastStyle } from "@raycast/api";
+import dayjs from "dayjs";
+
+export interface Author {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+}
+
+export interface Uploader {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+}
+
+export interface Asset {
+    url: string;
+    id: number;
+    node_id: string;
+    name: string;
+    label: string;
+    uploader: Uploader;
+    content_type: string;
+    state: string;
+    size: number;
+    download_count: number;
+    created_at: Date;
+    updated_at: Date;
+    browser_download_url: string;
+}
+
+export interface Reactions {
+    url: string;
+    total_count: number;
+    laugh: number;
+    hooray: number;
+    confused: number;
+    heart: number;
+    rocket: number;
+    eyes: number;
+}
+
+export interface Release {
+    url: string;
+    assets_url: string;
+    upload_url: string;
+    html_url: string;
+    id: number;
+    author: Author;
+    node_id: string;
+    tag_name: string;
+    target_commitish: string;
+    name: string;
+    draft: boolean;
+    prerelease: boolean;
+    created_at: Date;
+    published_at: Date;
+    assets: Asset[];
+    tarball_url: string;
+    zipball_url: string;
+    body: string;
+    reactions: Reactions;
+    mentions_count: number;
+}
+
+export default function main() {
+    const { releases, loading } = useSearch()
+
+    return (
+        <List
+            searchBarPlaceholder="Search releases by keyword..."
+            throttle={true}
+            isLoading={loading}
+            navigationTitle={'Check Halo releases'}
+        >
+            {releases?.map(release => (
+                <List.Item
+                    id={release.id.toString()}
+                    key={release.id}
+                    title={release.name}
+                    subtitle={`${getAssetsDownloadCount(release).toString()} downloads`}
+                    accessoryTitle={dayjs(release.created_at).format('YYYY-MM-DD')}
+                    icon={Icon.Dot}
+                    accessoryIcon={{ source: release.author.avatar_url, mask: ImageMask.Circle }}
+                    actions={
+                        <ActionPanel>
+                            <PushAction
+                                title="Show Details"
+                                target={<RenderReleaseDetail release={release} />}
+                            />
+                            <OpenInBrowserAction url={release.html_url} />
+                            {release.assets.map(asset => (
+                                <OpenInBrowserAction key={asset.id} url={asset.browser_download_url} title={`Download ${asset.name}`} />
+                            ))}
+                        </ActionPanel>
+                    }
+                />
+            ))}
+        </List>
+    )
+}
+
+export function RenderReleaseDetail(props: { release: Release }) {
+    const { release } = props
+
+    return (
+        <Detail
+            markdown={release?.body}
+            navigationTitle={release?.name}
+            actions={
+                <ActionPanel>
+                    <OpenInBrowserAction url={release?.html_url} />
+                </ActionPanel>
+            }
+        />
+    )
+}
+
+export function getAssetsDownloadCount(release: Release): number {
+    const { assets } = release
+    return assets.map(asset => asset.download_count).reduce((prev, current) => prev + current)
+}
+
+export function useSearch() {
+    const [releases, setReleases] = useState<Release[]>();
+    const [loading, setLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        async function fetchReleases() {
+            setLoading(true);
+            try {
+                const haloRestApiClient = new HaloRestAPIClient({
+                    baseUrl: "https://api.github.com",
+                });
+                const client = haloRestApiClient.buildHttpClient();
+                const response = await client.get("/repos/halo-dev/halo/releases", {});
+                setReleases(response)
+            } catch (error: any) {
+                showToast(ToastStyle.Failure, "Could not fetch halo github releases", error.message);
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchReleases()
+    }, [])
+
+    return { releases, loading }
+}
---
+++ src/journals.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import haloAdminClient from "./utils/api-client";
+import type { Journal } from "@halo-dev/admin-api";
+import { ActionPanel, Detail, ImageMask, List, PushAction, showToast, ToastStyle } from "@raycast/api";
+import dayjs from "dayjs";
+
+export default function main() {
+    const [keyword, setKeyword] = useState<string>();
+    const { journals, loading } = useSearch(keyword)
+
+    return (
+        <List
+            searchBarPlaceholder="Search journals by keyword..."
+            onSearchTextChange={setKeyword}
+            throttle={true}
+            isLoading={loading}
+            navigationTitle={'Search journals'}
+        >
+            <List.Section title={"Your journals"}>
+                {journals?.map(journal => (
+                    <List.Item
+                        id={journal.id?.toString()}
+                        key={journal.id}
+                        title={journal.sourceContent}
+                        subtitle={journal.type.toLowerCase()}
+                        accessoryTitle={dayjs(journal.createTime).format('YYYY-MM-DD')}
+                        icon={{ source: `https://ryanc.cc/avatar`, mask: ImageMask.Circle }}
+                        actions={
+                            <ActionPanel>
+                                <PushAction
+                                    title="Show Details"
+                                    target={<RenderJournalDetail journal={journal} />}
+                                />
+                            </ActionPanel>
+                        }
+                    />
+                ))}
+            </List.Section>
+        </List>
+    )
+}
+
+export function RenderJournalDetail(props: { journal: Journal }) {
+    const { journal } = props
+    return (
+        <Detail
+            markdown={journal?.sourceContent}
+            navigationTitle={dayjs(journal.createTime).format('YYYY-MM-DD')}
+        />
+    )
+}
+
+export function useSearch(keyword: string | undefined) {
+    const [journals, setJournals] = useState<Journal[]>();
+    const [loading, setLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        async function fetchJournals() {
+            setLoading(true);
+            try {
+                const response = await haloAdminClient.journal.list({
+                    keyword
+                })
+                setJournals(response.data.content)
+            } catch (error: any) {
+                showToast(ToastStyle.Failure, "Could not fetch journals", error.message);
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchJournals()
+    }, [keyword])
+
+    return { journals, loading }
+}
